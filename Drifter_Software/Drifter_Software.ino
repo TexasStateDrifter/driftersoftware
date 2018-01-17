aefcbb28264548e3fdc60229d75d79c6a9bc89cf
Isaac Bondoc <isaac.bondoc@gmail.com>
2018-01-17 05:50:24 -0600

    Drifter Software
    Inital code commit to MASTER branch. DO NOT modify code directly from MASTER branch. Make your own branch, sync, and then modify. You must SYNC before modifying your code before each time you start messing with the code, so that everything is updated.

---
+++ Drifter_Software/Drifter_Software.ino
@@ -0,0 +1,129 @@
+/**************************************************
+*            Drifter Software v1.0
+*
+* Programmers: Isaac Bondoc & John Marquez 
+* 
+* 
+* Description: Drifter software is developed to 
+* allow one to connect a Dissolved Oxygen, pH, 
+* temperature and conductivity sensor to sample 
+* water quality. A User interface was also developed
+* for easy interaction with the drifter.
+* 
+* 
+**************************************************/
+# include <LiquidCrystal.h> 
+
+
+LiquidCrystal lcd(8, 9, 4, 5, 6, 7);
+
+
+// define some values used by the panel and buttons
+int lcd_key     = 0;
+int adc_key_in  = 0;
+
+#define btnRIGHT  0
+#define btnUP     1
+#define btnDOWN   2
+#define btnLEFT   3
+#define btnSELECT 4
+#define btnNONE   5
+
+int read_LCD_buttons(){               // read the buttons
+    adc_key_in = analogRead(0);       // read the value from the sensor 
+
+    // my buttons when read are centered at these valies: 0, 144, 329, 504, 741
+    // we add approx 50 to those values and check to see if we are close
+    // We make this the 1st option for speed reasons since it will be the most likely result
+
+    if (adc_key_in > 1000) return btnNONE; 
+
+    // For V1.1 us this threshold
+    if (adc_key_in < 50)   return btnRIGHT;  
+    if (adc_key_in < 250)  return btnUP; 
+    if (adc_key_in < 450)  return btnDOWN; 
+    if (adc_key_in < 650)  return btnLEFT; 
+    if (adc_key_in < 850)  return btnSELECT;  
+
+    return btnNONE;                // when all others fail, return this.
+}
+
+void menuCursor(int d){
+  if (d > 0) { 
+    lcd.setCursor(0,1);
+    lcd.print(d);
+    lcd.setCursor(5, d - 1);
+    lcd.print(" ");
+    lcd.setCursor(5,d);
+    lcd.print("*");
+  }
+  else if (d == 0) {
+    lcd.setCursor(0,1);
+    lcd.print(d);
+    lcd.setCursor(5, d + 1);
+    lcd.print(" ");
+    lcd.setCursor(5,d);
+    lcd.print("*");
+  }
+
+
+}
+
+int Z = 1 ;
+
+void setup() {
+  // put your setup code here, to run once:
+  lcd.begin(16, 2);
+  lcd.setCursor(5, 0);
+  lcd.print("*Option 0");
+  lcd.setCursor(6,1);
+  lcd.print("Option 1");
+  Serial.begin(9600);
+}
+
+ 
+void loop() {
+  // put your main code here, to run repeatedly:
+   
+  lcd.setCursor(0,1);             // move to the begining of the second line
+ 
+ 
+  lcd_key = read_LCD_buttons();   // read the buttons
+  
+  switch (lcd_key){               // depending on which button was pushed, we perform an action
+    
+       case btnRIGHT:{             //  push button "RIGHT" and show the word on the screen
+       //     lcd.print("RIGHT ");
+            break;
+       }
+       case btnLEFT:{
+       //      lcd.print("LEFT   "); //  push button "LEFT" and show the word on the screen
+             break;
+       }    
+       case btnUP:{
+       //      lcd.print("UP    ");  //  push button "UP" and show the word on the screen
+             Z = Z - 1 ;
+             menuCursor(Z);
+             break;
+       }
+       case btnDOWN:{
+       //      lcd.print("DOWN  ");  //  push button "DOWN" and show the word on the screen
+             Z = Z + 1 ;
+             Serial.print(Z);
+             menuCursor(Z);
+
+             break;
+       }
+       case btnSELECT:{
+       //      lcd.print("SELECT");  //  push button "SELECT" and show the word on the screen
+             break;
+       }
+       case btnNONE:{
+       //      lcd.print("NONE  ");  //  No action  will show "None" on the screen
+           lcd.setCursor(0,1);
+   lcd.print(Z);
+             break;
+       }
+  }
+   
+}
