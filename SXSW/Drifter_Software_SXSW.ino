db635a46d7dc1c9db67ff42108ea3ae927e55820
Isaac Bondoc <isaac.bondoc@gmail.com>
2018-03-09 16:11:18 -0600

    Made seperate files for SXSW
    Copied current drifter software and changed name and added project folder in order to modify code for presenting, while not messing with other code.

---
+++ SXSW/Drifter_Software_SXSW.ino
@@ -0,0 +1,279 @@
+/***********************************************************
+*            Drifter Software v1.0
+*
+* Programmers: Isaac Bondoc & John Marquez 
+* 
+* 
+* Description: Drifter software is developed to 
+* allow one to connect a Dissolved Oxygen, pH, 
+* temperature and conductivity sensor to sample 
+* water quality. A User interface was also developed
+* for easy interaction with the drifter and its settings.
+* 
+* 
+************************************************************/
+#include <Wire.h>
+//#include <LowPower.h>
+#include <avr/pgmspace.h>
+#include <Adafruit_RGBLCDShield.h>
+#include <utility/Adafruit_MCP23017.h>
+#include <avr/sleep.h>
+
+Adafruit_RGBLCDShield lcd = Adafruit_RGBLCDShield();
+
+// These #define make it easy to set the backlight color.
+// Note: Can delete this whole section if you know you want
+//       to set it to particular color. Backlight is color
+//       is initialized in "void setup()".
+#define NONE 0x0
+#define RED 0x1
+#define YELLOW 0x3
+#define GREEN 0x2
+#define TEAL 0x6
+#define BLUE 0x4
+#define VIOLET 0x5
+#define WHITE 0x7
+
+#define MENU_COUNT 6 // Number of total MENU options -- Modify the number of rows in the array
+#define SUBMENU_COUNT 2 // Max # of SUBMENU options for any menu option
+bool menuMode = 0; // bool var to determine if it's in main menu (0) or sub menu(1). 
+
+// 2-dim Array of type String. If submenu option has less then max, fill in with (" ").
+String const menuOptions[MENU_COUNT][SUBMENU_COUNT+1] PROGMEM = {{"Option 0","subOpt00  ","subOpt01  "},
+                                                                 {"Sensors   ","Temp      ","subOpt11  "},
+                                                                 {"Option 2","subOpt20  ","subOpt21  "}, 
+                                                                 {"Sleep Mode","ON        ","subOpt31  "}, 
+                                                                 {"Option 4","subOpt40  ","subOpt41  "},
+                                                                 {"Backlight ","ON        ","OFF      "}};
+
+void setup() {
+  // setup code sets up the 1st page of the main menu
+  lcd.begin(16, 2);
+  lcd.setCursor(5, 0);
+  lcd.print("*");
+  lcd.print(menuOptions[0][0]);
+  lcd.setCursor(6,1);
+  lcd.print(menuOptions[1][0]);
+  Serial.begin(9600);
+  menuMode = 0;
+  lcd.setBacklight(GREEN);
+}
+
+// Menu Counter, Z, Gives you the position in the main menu
+// you are on. subMenu Counter, K, gives you the position in
+// the subMenu you are on.
+int Z = 0, K = 0; 
+
+// Main Program 
+void loop() {
+ 
+  lcd.setCursor(0,0);             // move to the begining of the second line
+
+  uint8_t lcd_key = lcd.readButtons();  // Read the buttons 
+  
+  switch (lcd_key){               // depending on which button was pushed, we perform an action
+    
+       case BUTTON_RIGHT:{             //  push button "RIGHT" and show the word on the screen
+          //  lcd.print(F("RIGHT"));
+            delay(300);
+            break;
+       }
+       case BUTTON_LEFT:{
+           //  lcd.print(F("LEFT ")); //  push button "LEFT" and show the word on the screen
+             Z = menuLeft(Z);
+             delay(300);
+             break;
+       }    
+       case BUTTON_UP:{
+          //   lcd.print(F("UP   "));  //  push button "UP" and show the word on the screen
+             Z = menuUp(Z);
+             delay(300);
+             break;
+       }
+       case BUTTON_DOWN:{
+           //  lcd.print(F("DOWN "));  //  push button "DOWN" and show the word on the screen
+             Z = menuDown(Z);
+             delay(300);
+             break;      
+       }
+       case BUTTON_SELECT:{
+           //  lcd.print(F("SEL  "));  //  push button "SELECT" and show the word on the screen
+             Z = menuSel(Z);
+             delay(300);
+             break;
+       }
+       default :{
+          // lcd.print(F("NONE "));  //  No action  will show "None" on the screen
+           lcd.setCursor(0,1);
+           lcd.print(Z);
+           break;
+       }
+  }
+}
+
+//////////////////////////////////////////////////////////////////
+// Function: menuCursor takes in a parameter "d" from one of the 
+//           up, down, left, or select functions. "d" indicates 
+//           the main menu position so that the UI can be on 
+//           the right menu page. 
+//////////////////////////////////////////////////////////////////
+void menuCursor(int d){
+
+  // Clear the astick area. Note: I could have just used lcd.clear()
+  // but using it makes the screen perform a noticable flicker.
+  lcd.setCursor(5,0);
+  lcd.print(F(" "));
+  lcd.setCursor(5,1);
+  lcd.print(F(" "));
+  lcd.setCursor(6,0);
+  
+  if(menuMode == 0){  // If in main menu mode
+    lcd.setCursor(5, d % 2);     // Set cursor to place astrick
+    lcd.print(F("*"));
+    lcd.setCursor(6,0);
+   
+    if(d <= MENU_COUNT and d % 2 == 1){  // If menu option and odd position number
+     lcd.print(menuOptions[d-1][0]);
+     lcd.setCursor(6,1);
+     lcd.print(menuOptions[d][0]);
+    }
+    else if(d <=  MENU_COUNT and d % 2 == 0){  // If menu option and even position number
+     lcd.print(menuOptions[d][0]);
+     lcd.setCursor(6,1);
+     lcd.print(menuOptions[d+1][0]);
+    } 
+  }
+  
+  else if(menuMode == 1) { // If in sub menu mode
+     lcd.setCursor(5, (K-1) % 2);
+     lcd.print(F("*"));
+     
+    if(K % 2 == 1){  // If menu option and even position number
+      lcd.setCursor(6,0);
+      lcd.print(menuOptions[d][K]);
+      lcd.setCursor(6,1);
+      lcd.print(menuOptions[d][K+1]);
+    }
+    else if(K % 2 == 0){  // If sub option and odd position number
+      lcd.setCursor(6,0);
+      lcd.print(menuOptions[d][K-1]);
+      lcd.setCursor(6,1);
+      lcd.print(menuOptions[d][K]);
+    }
+  }
+ 
+}
+
+// Pressing up button will decrement menu counter. 
+int menuUp(int i) {
+    
+    if(menuMode == 0){  // If in main menu 
+        i -= 1;         
+      if(i + 1 <= 1){
+        i = 0 ;
+      }
+    }
+    else if(menuMode == 1){ // Else if in sub menu
+      K -= 1 ;
+      if(K == 0){
+        K = 1;    
+      }
+    }
+    
+    lcd.setCursor(0,1);
+    lcd.print(i);
+    menuCursor(i);
+    return i;
+}
+
+// Pressing down button increase counter by 1. 
+int menuDown(int i) {
+    
+    if(menuMode == 0){  // If in Main menu 
+      i += 1;
+      if (i == MENU_COUNT) {
+        i = MENU_COUNT - 1; 
+      }
+    }
+    else if(menuMode == 1){ // If in Sub menu
+      K += 1;
+      if(K == SUBMENU_COUNT){
+        K = SUBMENU_COUNT ;
+      }
+      else{  
+        K -= 1;
+      }
+    }
+    lcd.setCursor(0,1);
+    lcd.print(i);
+    menuCursor(i);
+    return i;
+}
+
+int menuSel(int i){
+  // If in mainMenu, put in SubMenu mode (mainMenu == 1) 
+  // Increment K (position in sub menu)
+  if (menuMode == 0) {
+    menuMode = 1;
+    K += 1 ;
+    lcd.setCursor(0,1);
+    lcd.print(i);
+    menuCursor(i);  
+    return i;
+  }
+  // Else you are already in subMenu and want to trigger 
+  // an action
+  else{
+    if(Z == 1 && K == 1){ // If selected 1st suboption of the 1st main menu option run this.
+      Serial.print(F("This is right before the dataLog code for the temp sensor"));  // Debug code
+      RTCdatalog();
+      lcd.clear();
+      lcd.print(getTemp());
+      delay(5000);
+      lcd.setCursor(0,0);
+      lcd.print("        ");
+    }
+    else if(Z == 1 && K == 2){
+      // Do something
+    }
+    else if(Z == 2 && K == 1){
+      // Do something
+    }
+    else if(Z == 2 && K == 2){
+      // Do something
+    }
+    else if(Z == 3 && K == 1){
+      // Do something
+      set_sleep_mode (SLEEP_MODE_PWR_DOWN);  
+      sleep_enable();
+      sleep_cpu ();  
+    }
+    else if(Z == 3 && K == 2){
+      // Do something
+    }    
+    else if(Z == 4 && K == 1){
+      lcd.display();
+    }
+    else if(Z == 4 && K ==2){
+      lcd.noDisplay();
+    }
+    else if(Z == 5 && K == 1){
+      lcd.setBacklight(GREEN); // Turn backlight ON
+    }
+    else if(Z == 5 && K == 2) {
+      lcd.setBacklight(NONE); // Turn backlight OFF
+    }
+  }
+}
+
+int menuLeft(int i){
+  menuMode = 0;
+  K = 0 ;
+  i = 0 ;
+  lcd.setCursor(0,1);
+  lcd.print(i);
+  menuCursor(i);
+  return(i);
+}
+
+
---
+++ SXSW/StoreDataMeasured.ino
@@ -0,0 +1,157 @@
+#include <SPI.h>
+#include <SD.h>
+#include <OneWire.h>
+#include <Wire.h>
+#include "RTClib.h"
+
+
+File dataFile;
+RTC_DS1307 rtc;
+
+char const daysOfTheWeek[7][12] PROGMEM = {"Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"};
+double temp_F;
+int DS18S20_Pin = 2; //DS18S20 Signal pin on digital 2
+
+//Temperature chip i/o
+OneWire ds(DS18S20_Pin);  // on digital pin 2
+
+void RTCsetup() 
+{
+  //The serial monitor output will only be read in 9600
+  Serial.begin(9600);
+  if (! rtc.begin()) {
+    Serial.println(F("Couldn't find RTC"));
+      while (1);
+  }
+  
+  while (!Serial) {
+    ;
+  }
+
+  Serial.print(F("Initializing SD card...."));
+  if (!SD.begin(10)) {
+    Serial.println(F("initialization failed!"));
+    while (1);
+  }
+  Serial.println(F("initialization done."));
+
+  if (!rtc.isrunning()) {
+    Serial.println(F("RTC is NOT running!"));
+    // following line sets the RTC to the date & time this sketch was compiled
+    // rtc.adjust(DateTime(F(__DATE__), F(__TIME__)));
+    // This line sets the RTC with an explicit date & time, for example to set
+    // Feburary 9, 2018 at 2pm you would call:
+    rtc.adjust(DateTime(2018, 2, 12, 14, 41, 0));
+  }
+}
+
+void RTCdatalog() 
+{
+    dataFile = SD.open("temp.txt", FILE_WRITE);
+
+    if(dataFile)
+    {
+      float temperature = getTemp();
+      DateTime now = rtc.now();
+      
+      Serial.print(temperature);
+      dataFile.print(temperature);
+      Serial.print(F("°C"));
+      dataFile.print(F("°C"));
+      Serial.print(F(" "));
+      dataFile.print(F(" "));
+      temp_F = ((temperature * 1.8) + 32);
+      Serial.print(temp_F);
+      dataFile.print(temp_F);
+      Serial.print(F("°F"));
+      dataFile.print(F("°F"));
+
+      Serial.print(F("   "));
+      dataFile.print(F("   "));
+
+      
+      Serial.print(now.year(), DEC);
+      Serial.print(F("/"));
+      Serial.print(now.month(), DEC);
+      Serial.print(F("/"));
+      Serial.print(now.day(), DEC);
+      Serial.print(F(" ("));
+      Serial.print(daysOfTheWeek[now.dayOfTheWeek()]);
+      Serial.print(F(") "));
+      Serial.print(now.hour(), DEC);
+      Serial.print(F(":"));
+      Serial.print(now.minute(), DEC);
+      Serial.print(F(":"));
+      Serial.print(now.second(), DEC);
+      Serial.println();
+      
+      dataFile.print(now.year(), DEC);
+      dataFile.print('/');
+      dataFile.print(now.month(), DEC);
+      dataFile.print('/');
+      dataFile.print(now.day(), DEC);
+      dataFile.print(" (");
+      dataFile.print(daysOfTheWeek[now.dayOfTheWeek()]);
+      dataFile.print(") ");
+      dataFile.print(now.hour(), DEC);
+      dataFile.print(':');
+      dataFile.print(now.minute(), DEC);
+      dataFile.print(':');
+      dataFile.print(now.second(), DEC);
+      dataFile.println(); 
+
+      dataFile.close();
+    }
+    else
+    {
+      Serial.println(F("error opening temp.txt"));
+    }
+    // delay(5000); //slow sample speed 15 seconds
+}
+
+float getTemp(){
+  //returns the temperature from one DS18S20 in DEG Celsius
+
+  byte data[12];
+  byte addr[8];
+
+  if ( !ds.search(addr)) {
+      //no more sensors on chain, reset search
+      ds.reset_search();
+      return -1000;
+  }
+
+  if ( OneWire::crc8( addr, 7) != addr[7]) {
+      Serial.println(F("CRC is not valid!"));
+      return -1000;
+  }
+
+  if ( addr[0] != 0x10 && addr[0] != 0x28) {
+      Serial.print(F("Device is not recognized"));
+      return -1000;
+  }
+
+  ds.reset();
+  ds.select(addr);
+  ds.write(0x44,1); // start conversion, with parasite power on at the end
+
+  byte present = ds.reset();
+  ds.select(addr);    
+  ds.write(0xBE); // Read Scratchpad
+
+  
+  for (int i = 0; i < 9; i++) { // we need 9 bytes
+    data[i] = ds.read();
+  }
+  
+  ds.reset_search();
+  
+  byte MSB = data[1];
+  byte LSB = data[0];
+
+  float tempRead = ((MSB << 8) | LSB); //using two's compliment
+  float TemperatureSum = tempRead / 16;
+  
+  return TemperatureSum;
+  
+}
