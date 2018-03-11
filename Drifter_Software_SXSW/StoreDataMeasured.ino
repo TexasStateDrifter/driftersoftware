d6fbe28560246192d9e27b48a1baee3859676cbf
JohnMarquez <35536915+JohnMarquez@users.noreply.github.com>
2018-03-10 20:19:07 -0600

    update

--- Drifter_Software_SXSW/StoreDataMeasured.ino
+++ Drifter_Software_SXSW/StoreDataMeasured.ino
@@ -1,22 +1,30 @@
 #include <SPI.h>
 #include <SD.h>
 //#include <OneWire.h>
-//#include <Wire.h>
+#include <Wire.h>
 #include "RTClib.h"
 
+//datalogger A4 A5
+//DO sensor A1
+//EC sensor rx 2 tx 3
+//pH sensor A0
 
+#define DoSensorPin  A1
 File dataFile;
+File dataFile2;
 //RTC_DS1307 rtc;
 RTC_PCF8523 rtc;
 
-char const daysOfTheWeek[7][12] PROGMEM = {"Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"};
+//char const daysOfTheWeek[7][12] PROGMEM = {"Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"};
+char daysOfTheWeek[7][12] = {"Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"};
 double temp_F;
 //int DS18S20_Pin = 2; //DS18S20 Signal pin on digital 2
 
 //Temperature chip i/o
 //OneWire ds(DS18S20_Pin);  // on digital pin 2
 
-void RTCsetup() 
+//void RTCsetup()
+void setup() 
 {
   //The serial monitor output will only be read in 9600
   Serial.begin(57600);
@@ -24,6 +32,11 @@ void RTCsetup()
     Serial.println(F("Couldn't find RTC"));
       while (1);
   }
+  
+  // DO_Read
+  pinMode(DoSensorPin,INPUT);
+  readDoCharacteristicValues();
+  //
   
   while (!Serial) {
     ;
@@ -42,17 +55,19 @@ void RTCsetup()
     // rtc.adjust(DateTime(F(__DATE__), F(__TIME__)));
     // This line sets the RTC with an explicit date & time, for example to set
     // Feburary 9, 2018 at 2pm you would call:
-    rtc.adjust(DateTime(2018, 2, 12, 14, 41, 0));
+    rtc.adjust(DateTime(2018, 3, 9, 17, 59, 0));
   }
 }
 
-void RTCdatalog() 
+//void RTCdatalog() 
+void loop()
 {
     dataFile = SD.open("temp.txt", FILE_WRITE);
-
-    if(dataFile)
+    dataFile2 = SD.open("do.txt", FILE_WRITE);
+    if(dataFile && dataFile2)
     {
       float temperature = getTemp();
+      float doRead = getDO();
       DateTime now = rtc.now();
       
       Serial.print(temperature);
@@ -70,6 +85,14 @@ void RTCdatalog()
       Serial.print(F("   "));
       dataFile.print(F("   "));
 
+      Serial.print(doRead);
+      dataFile2.print(doRead);
+      Serial.print(F(" "));
+      dataFile2.print(F(" "));
+      Serial.print(F("mg/L"));
+      dataFile2.print(F("mg/L"));
+      Serial.print(F("   "));
+      dataFile2.print(F("   "));
       
       Serial.print(now.year(), DEC);
       Serial.print(F("/"));
@@ -99,15 +122,31 @@ void RTCdatalog()
       dataFile.print(now.minute(), DEC);
       dataFile.print(':');
       dataFile.print(now.second(), DEC);
-      dataFile.println(); 
+      dataFile.println();
+
+      dataFile2.print(now.year(), DEC);
+      dataFile2.print('/');
+      dataFile2.print(now.month(), DEC);
+      dataFile2.print('/');
+      dataFile2.print(now.day(), DEC);
+      dataFile2.print(" (");
+      dataFile2.print(daysOfTheWeek[now.dayOfTheWeek()]);
+      dataFile2.print(") ");
+      dataFile2.print(now.hour(), DEC);
+      dataFile2.print(':');
+      dataFile2.print(now.minute(), DEC);
+      dataFile2.print(':');
+      dataFile2.print(now.second(), DEC);
+      dataFile2.println(); 
 
       dataFile.close();
+      dataFile2.close();
     }
     else
     {
       Serial.println(F("error opening temp.txt"));
     }
-    // delay(5000); //slow sample speed 15 seconds
+    delay(5000); //slow sample speed 15 seconds
 }
 
 /*
