ac98d3fe1033ae612d15631dd88a1167d2894d1d
Isaac Bondoc <isaac.bondoc@gmail.com>
2018-04-04 11:35:50 -0500

    Added options
    but didn't added the functionality

--- Drifter_Software_SXSW/Drifter_Software_SXSW.ino
+++ Drifter_Software_SXSW/Drifter_Software_SXSW.ino
@@ -12,53 +12,6 @@
 * 
 * 
 ************************************************************/
-
-/*
-#include <avr/pgmspace.h>
-#include <EEPROM.h>
-
-#define DoSensorPin  A1
-//dissolved oxygen sensor analog output pin to arduino mainboard
-#define VREF 5000    //for arduino uno, the ADC reference is the AVCC, that is 5000mV(TYP)
-float doValue;      //current dissolved oxygen value, unit; mg/L
-float temperature = 25;    //default temperature is 25^C, you can use a temperature sensor to read it
-
-#define EEPROM_write(address, p) {int i = 0; byte *pp = (byte*)&(p);for(; i < sizeof(p); i++) EEPROM.write(address+i, pp[i]);}
-#define EEPROM_read(address, p)  {int i = 0; byte *pp = (byte*)&(p);for(; i < sizeof(p); i++) pp[i]=EEPROM.read(address+i);}
-
-#define ReceivedBufferLength 20
-char receivedBuffer[ReceivedBufferLength+1];    // store the serial command
-byte receivedBufferIndex = 0;
-
-#define SCOUNT  30           // sum of sample point
-int analogBuffer[SCOUNT];    //store the analog value in the array, readed from ADC
-int analogBufferTemp[SCOUNT];
-int analogBufferIndex = 0,copyIndex = 0;
-
-#define SaturationDoVoltageAddress 12          //the address of the Saturation Oxygen voltage stored in the EEPROM
-#define SaturationDoTemperatureAddress 16      //the address of the Saturation Oxygen temperature stored in the EEPROM
-float SaturationDoVoltage,SaturationDoTemperature;
-float averageVoltage;
-
-const float SaturationValueTab[41] PROGMEM = {      //saturation dissolved oxygen concentrations at various temperatures
-14.46, 14.22, 13.82, 13.44, 13.09,
-12.74, 12.42, 12.11, 11.81, 11.53,
-11.26, 11.01, 10.77, 10.53, 10.30,
-10.08, 9.86,  9.66,  9.46,  9.27,
-9.08,  8.90,  8.73,  8.57,  8.41,
-8.25,  8.11,  7.96,  7.82,  7.69,
-7.56,  7.43,  7.30,  7.18,  7.07,
-6.95,  6.84,  6.73,  6.63,  6.53,
-6.41,
-};
-*/
-
-
-
-
-
-
-
 #include <Wire.h>
 //#include <LowPower.h>
 #include <avr/pgmspace.h>
@@ -76,20 +29,26 @@ Adafruit_RGBLCDShield lcd = Adafruit_RGBLCDShield();
 #define NONE    0x0
 #define GREEN   0x2
 
-#define MENU_COUNT 3 // Number of total MENU options -- Modify the number of rows in the array
-#define SUBMENU_COUNT 3 // Max # of SUBMENU options for any menu option
+#define MENU_COUNT 7 // Number of total MENU options -- Modify the number of rows in the array
+#define SUBMENU_COUNT 4 // Max # of SUBMENU options for any menu option
 bool menuMode = 0; // bool var to determine if it's in main menu (0) or sub menu(1). 
 
 // 2-dim Array of type String. If submenu option has less then max, fill in with (" ").
-String const menuOptions[MENU_COUNT+1][SUBMENU_COUNT+1] PROGMEM = {{"Sample Now","subOpt00  ","subOpt01  ","          "},
-                                                                   {"Sensors   ","Temp      ","pH        ","D.O.      "},
-                                                                   {"Backlight ","ON        ","OFF       ","          "},
-                                                                   {"          ","          ","          ","          "}};
-                                                                // {"Option 4  ","subOpt40  ","subOpt41  "}
-                                                                // {"Backlight ","ON        ","OFF      "}};
+String const menuOptions[MENU_COUNT+1][SUBMENU_COUNT+1] PROGMEM = {{"Run       ","          ","          ","          ","          "},
+                                                                   {"Sample Now","          ","          ","          ","          "},
+                                                                   {"Sensors   ","Temp      ","pH        ","Cond.     ","D.O       "},
+                                                                   {"SampleFreq","Mod. Rate ","          ","          ","          "},
+                                                                   {"Time/Date ","Mod. Time ","Mod. Date ","          ","          "},
+                                                                   {"SleepMode ","ON        ","OFF       ","          ","          "},
+                                                                   {"Backlight ","ON        ","OFF       ","          ","          "},
+                                                                   {"          ","          ","          ","          ","          "}};
+
+// String const subOptions[][] PROGMEM = {{"",""},
+//                                       {"",""},
+//                                       {"",""}};
 
 void setup() {
-  // setup code sets up the 1st page of the main menu
+// setup code sets up the 1st page of the main menu
 //  RTCsetup();
 //  funcSetup();
   lcd.begin(16, 2);
@@ -100,12 +59,10 @@ void setup() {
   lcd.print(menuOptions[1][0]);
   Serial.begin(9600);
   menuMode = 0;
-  lcd.setBacklight(GREEN);
-  
+  lcd.setBacklight(0x7);
   pinMode(DoSensorPin,INPUT);
   readDoCharacteristicValues();
 }
-
 // Menu Counter, Z, Gives you the position in the main menu
 // you are on. subMenu Counter, K, gives you the position in
 // the subMenu you are on.
@@ -315,6 +272,12 @@ int menuSel(int i){
   // Else you are already in subMenu and want to trigger 
   // an action
   else if(menuMode == 0 and Z == 0){      
+   // The code below will put arduino into sleep mode
+   // in which it will be waken up by an external inturrupt
+   
+  }
+  else{
+    if(Z == 1 and K == 1){ // If selected 1st suboption of the 1st main menu option run this.
       lcd.clear();
       lcd.print("Temp:");
       lcd.print(getTemp());
@@ -326,21 +289,6 @@ int menuSel(int i){
       lcd.clear();
       lcd.print("pH:");
       lcd.print(getPH());
-      delay(3000);
-      lcd.setCursor(0,0);
-      lcd.print(F("        "));
-      lcd.setCursor(5, 0);
-      lcd.print("*");
-      lcd.print(menuOptions[0][0]);
-      lcd.setCursor(6,1);
-      lcd.print(menuOptions[1][0]);
-  }
-  else{
-    if(Z == 1 and K == 1){ // If selected 1st suboption of the 1st main menu option run this.
-      Serial.print(F("This is right before the dataLog code for the temp sensor"));  // Debug code
-      lcd.clear();
-      lcd.print("Temp:");
-      lcd.print(getTemp());
       delay(3000);
       lcd.setCursor(0,0);
       lcd.print(F("        "));
@@ -418,123 +366,4 @@ int menuLeft(int i){
   menuCursor(i);
   return(i);
 }
-
-
-
-
-
-
-
-
-
-/*
-float getDO()
-{
-   static unsigned long analogSampleTimepoint = millis();
-   if(millis()-analogSampleTimepoint > 30U)     //every 30 milliseconds,read the analog value from the ADC
-   {
-     analogSampleTimepoint = millis();
-     analogBuffer[analogBufferIndex] = analogRead(DoSensorPin);    //read the analog value and store into the buffer
-     analogBufferIndex++;
-     if(analogBufferIndex == SCOUNT) 
-         analogBufferIndex = 0;
-   }
-   
-   static unsigned long tempSampleTimepoint = millis();
-   if(millis()-tempSampleTimepoint > 500U)  // every 500 milliseconds, read the temperature
-   {
-      tempSampleTimepoint = millis();
-      //temperature = readTemperature();  // add your temperature codes here to read the temperature, unit:^C
-   }
-   
-   static unsigned long printTimepoint = millis();
-   if(millis()-printTimepoint > 1000U)
-   {
-      printTimepoint = millis();
-      for(copyIndex=0;copyIndex<SCOUNT;copyIndex++)
-      {
-        analogBufferTemp[copyIndex]= analogBuffer[copyIndex];
-      }
-      averageVoltage = getMedianNum(analogBufferTemp,SCOUNT) * (float)VREF / 1024.0; // read the value more stable by the median filtering algorithm
-      //Serial.print(F("Temperature:"));
-      //Serial.print(temperature,1);
-      //Serial.print(F("^C"));
-      doValue = pgm_read_float_near( &SaturationValueTab[0] + (int)(SaturationDoTemperature+0.5) ) * averageVoltage / SaturationDoVoltage;  //calculate the do value, doValue = Voltage / SaturationDoVoltage * SaturationDoValue(with temperature compensation)
-      //Serial.print(F("DO Value:"));
-      //Serial.print(doValue,2);
-      //Serial.println(F("mg/L"));
-      return doValue;
-   }
- 
-}
-
-boolean serialDataAvailable(void)
-{
-  char receivedChar;
-  static unsigned long receivedTimeOut = millis();
-  while ( Serial.available() > 0 ) 
-  {   
-    if (millis() - receivedTimeOut > 500U) 
-    {
-      receivedBufferIndex = 0;
-      memset(receivedBuffer,0,(ReceivedBufferLength+1));
-    }
-    receivedTimeOut = millis();
-    receivedChar = Serial.read();
-    if (receivedChar == '\n' || receivedBufferIndex == ReceivedBufferLength)
-    {
-  receivedBufferIndex = 0;
-  strupr(receivedBuffer);
-  return true;
-    }else{
-        receivedBuffer[receivedBufferIndex] = receivedChar;
-        receivedBufferIndex++;
-    }
-  }
-  return false;
-}
-
-int getMedianNum(int bArray[], int iFilterLen) 
-{
-      int bTab[iFilterLen];
-      for (byte i = 0; i<iFilterLen; i++)
-      {
-    bTab[i] = bArray[i];
-      }
-      int i, j, bTemp;
-      for (j = 0; j < iFilterLen - 1; j++) 
-      {
-    for (i = 0; i < iFilterLen - j - 1; i++) 
-          {
-      if (bTab[i] > bTab[i + 1]) 
-            {
-    bTemp = bTab[i];
-          bTab[i] = bTab[i + 1];
-    bTab[i + 1] = bTemp;
-       }
-    }
-      }
-      if ((iFilterLen & 1) > 0)
-  bTemp = bTab[(iFilterLen - 1) / 2];
-      else
-  bTemp = (bTab[iFilterLen / 2] + bTab[iFilterLen / 2 - 1]) / 2;
-      return bTemp;
-}
-
-void readDoCharacteristicValues(void)
-{
-    EEPROM_read(SaturationDoVoltageAddress, SaturationDoVoltage);  
-    EEPROM_read(SaturationDoTemperatureAddress, SaturationDoTemperature);
-    if(EEPROM.read(SaturationDoVoltageAddress)==0xFF && EEPROM.read(SaturationDoVoltageAddress+1)==0xFF && EEPROM.read(SaturationDoVoltageAddress+2)==0xFF && EEPROM.read(SaturationDoVoltageAddress+3)==0xFF)
-    {
-      SaturationDoVoltage = 1127.6;   //default voltage:1127.6mv
-      EEPROM_write(SaturationDoVoltageAddress, SaturationDoVoltage);
-    }
-    if(EEPROM.read(SaturationDoTemperatureAddress)==0xFF && EEPROM.read(SaturationDoTemperatureAddress+1)==0xFF && EEPROM.read(SaturationDoTemperatureAddress+2)==0xFF && EEPROM.read(SaturationDoTemperatureAddress+3)==0xFF)
-    {
-      SaturationDoTemperature = 25.0;   //default temperature is 25^C
-      EEPROM_write(SaturationDoTemperatureAddress, SaturationDoTemperature);
-    }    
-}
-*/
 
--- sensor sample code/arduino_mega_EC_sample_code/arduino_mega_EC_sample_code.ino
+++ sensor sample code/arduino_mega_EC_sample_code/arduino_mega_EC_sample_code.ino
@@ -4,14 +4,7 @@
 //This code will output data to the Arduino serial monitor.
 //Type commands into the Arduino serial monitor to control the EC circuit.
 //This code was written in the Arduino 1.6.5 IDE
-//An Arduino UNO was used to test this code.
-
-
-#include <SoftwareSerial.h>                           //we have to include the SoftwareSerial library, or else we can't use it
-#define rx 2                                          //define what pin rx is going to be
-#define tx 3                                          //define what pin tx is going to be
-
-SoftwareSerial myserial(rx, tx);                      //define how the soft serial port is going to work
+//An Arduino MEGA was used to test this code.
 
 
 String inputstring = "";                              //a string to hold incoming data from the PC
@@ -20,11 +13,9 @@ boolean input_string_complete = false;                //have we received all the
 boolean sensor_string_complete = false;               //have we received all the data from the Atlas Scientific product
 
 
-
-
 void setup() {                                        //set up the hardware
   Serial.begin(9600);                                 //set baud rate for the hardware serial port_0 to 9600
-  myserial.begin(9600);                               //set baud rate for the software serial port to 9600
+  Serial3.begin(9600);                                //set baud rate for software serial port_3 to 9600
   inputstring.reserve(10);                            //set aside some bytes for receiving data from the PC
   sensorstring.reserve(30);                           //set aside some bytes for receiving data from Atlas Scientific product
 }
@@ -36,23 +27,21 @@ void serialEvent() {                                  //if the hardware serial p
 }
 
 
+void serialEvent3() {                                 //if the hardware serial port_3 receives a char
+  sensorstring = Serial3.readStringUntil(13);         //read the string until we see a <CR>
+  sensor_string_complete = true;                      //set the flag used to tell if we have received a completed string from the PC
+}
+
+
 void loop() {                                         //here we go...
 
-  if (input_string_complete) {                        //if a string from the PC has been received in its entirety
-    myserial.print(inputstring);                      //send that string to the Atlas Scientific product
-    myserial.print('\r');                             //add a <CR> to the end of the string
+
+  if (input_string_complete == true) {                //if a string from the PC has been received in its entirety
+    Serial3.print(inputstring);                       //send that string to the Atlas Scientific product
+    Serial3.print('\r');                              //add a <CR> to the end of the string
     inputstring = "";                                 //clear the string
     input_string_complete = false;                    //reset the flag used to tell if we have received a completed string from the PC
   }
-
-  if (myserial.available() > 0) {                     //if we see that the Atlas Scientific product has sent a character
-    char inchar = (char)myserial.read();              //get the char we just received
-    sensorstring += inchar;                           //add the char to the var called sensorstring
-    if (inchar == '\r') {                             //if the incoming character is a <CR>
-      sensor_string_complete = true;                  //set the flag
-    }
-  }
-
 
   if (sensor_string_complete == true) {               //if a string from the Atlas Scientific product has been received in its entirety
     if (isdigit(sensorstring[0]) == false) {          //if the first character in the string is a digit
@@ -60,7 +49,7 @@ void loop() {                                         //here we go...
     }
     else                                              //if the first character in the string is NOT a digit
     {
-      print_EC_data();                                //then call this function 
+      print_EC_data();                                //then call this function
     }
     sensorstring = "";                                //clear the string
     sensor_string_complete = false;                   //reset the flag used to tell if we have received a completed string from the Atlas Scientific product
@@ -68,8 +57,7 @@ void loop() {                                         //here we go...
 }
 
 
-
-void print_EC_data(void) {                            //this function will pars the string  
+void print_EC_data(void) {                            //this function will pars the string
 
   char sensorstring_array[30];                        //we make a char array
   char *EC;                                           //char pointer used in string parsing
@@ -77,8 +65,8 @@ void print_EC_data(void) {                            //this function will pars 
   char *SAL;                                          //char pointer used in string parsing
   char *GRAV;                                         //char pointer used in string parsing
   float f_ec;                                         //used to hold a floating point number that is the EC
-  
-  sensorstring.toCharArray(sensorstring_array, 30);   //convert the string to a char array 
+
+  sensorstring.toCharArray(sensorstring_array, 30);   //convert the string to a char array
   EC = strtok(sensorstring_array, ",");               //let's pars the array at each comma
   TDS = strtok(NULL, ",");                            //let's pars the array at each comma
   SAL = strtok(NULL, ",");                            //let's pars the array at each comma
@@ -95,27 +83,7 @@ void print_EC_data(void) {                            //this function will pars 
 
   Serial.print("GRAV:");                              //we now print each value we parsed separately
   Serial.println(GRAV);                               //this is the specific gravity
-  Serial.println();                                   //this just makes the output easier to read
-  
+  Serial.println();                                   //this just makes the output easer to read
+
 //f_ec= atof(EC);                                     //uncomment this line to convert the char to a float
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
